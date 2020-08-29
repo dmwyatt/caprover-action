a06c4de17a0de2f4ec2e42ca3c4af6792f53d9e2
Dustin Wyatt <dustin.wyatt@gmail.com>
2020-08-29 15:37:13 -0500

    Built artifacts are in subdir

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
-tar -cvf ./deploy.tar ./captain-definition *
+tar -cvf ./deploy.tar ./captain-definition ./build/*
 caprover deploy -h $INPUT_SERVER -p $INPUT_PASSWORD -b $INPUT_BRANCH -a $INPUT_APPNAME 
